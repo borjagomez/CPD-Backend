3b10a4ea5c80c4fc2a1ac86cf539c09d67fe1554
Borja Gómez Muñoz <borja@reverscore.com>
2021-05-31 21:05:41 +0200

    NPM install on docker-compose

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "CPD" });
+  res.render("index", { title: "CPD 123456" });
 });
 
 module.exports = router;
