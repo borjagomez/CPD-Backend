ac753f37d14440c986117264cb2d80e2bfdf6da3
Borja Gómez Muñoz <borja@reverscore.com>
2021-06-08 20:24:47 +0200

    Adding GCloud bucket upload

--- app/controllers/cpdController.js
+++ app/controllers/cpdController.js
@@ -1,5 +1,29 @@
 const { v4: uuidv4 } = require("uuid");
+const R = require("r-script");
+const { Storage } = require("@google-cloud/storage");
+
+const bucketName = "cpd-images";
 
 exports.generateCPD = (req, res) => {
-  res.json({ cpdid: uuidv4() });
+  const storage = new Storage({ keyFilename: "./cpd-storage.json" });
+  const cpdid = uuidv4();
+  const destination = `${cpdid}.png`;
+
+  try {
+    R("CPD/streamBTC.R").callSync();
+  } catch (err) {
+    console.log(err);
+  }
+
+  async function uploadFile() {
+    await storage.bucket(bucketName).upload("CPD.png", {
+      destination,
+    });
+
+    console.log(`${destination} uploaded to ${bucketName}`);
+  }
+
+  uploadFile().catch(console.error);
+
+  res.json({ cpdid });
 };
